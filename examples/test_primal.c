f5128967a855688bd02612a32f5bb412287d5329
Michael Perk <perk@ibr.cs.tu-bs.de>
2025-01-28 15:51:07 +0100

    Fixing a test case s.t. GLPK now build again

--- examples/test_primal.c
+++ examples/test_primal.c
@@ -2,22 +2,26 @@
 #include "callbacks/callbacks.h"
 
 void iteration_data_cb(int a,
-                        double * b, int * c, int * d, int e, // basis
-                        //double * f, int * g, int * h, int i, //inverse
-                        int j, double k, double l) {
+                       double *b, int *c, int *d, int e, // basis
+                       double *j_, int j, double k, // max reduced cost
+                       double l, // condition number (under 1-norm),
+                       double k_, // 1-norm AB,
+                       double l_ // 1-norm AB^-1
+) {
     xprintf("CALLBACK REACHED");
 }
 
 void initial_data_cb(int a, int b,
-                     double * c,
-                     double * d, int * e, int * f, int g,
-                     double * h){
+                     double *c,
+                     double *d, int *e, int *f, int g,
+                     double *h) {
     xprintf("INITIAL");
 }
 
-void new_iteration(){
+void new_iteration() {
     xprintf("NEW IT");
 }
+
 int main(int argc, char *argv[]) {
     setInitialDataCallback(initialDataCallback);
     setNewIterationCallback(new_iteration);
